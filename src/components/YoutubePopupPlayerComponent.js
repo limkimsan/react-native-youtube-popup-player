fee55cb6707ce0da86b7891ea8d40f612d918127
Kimsanlim <kimsanlim27@gmail.com>
2023-03-27 14:27:41 +0700

    Apply the react-native-youtube-iframe-player

--- src/components/YoutubePopupPlayerComponent.js
+++ src/components/YoutubePopupPlayerComponent.js
@@ -1,27 +1,31 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import {View} from 'react-native';
 import Modal from 'react-native-modal';
-// import YoutubePlayer from "react-native-youtube-iframe";
+import NetInfo from '@react-native-community/netinfo'
 import YoutubeIframePlayer from 'react-native-youtube-iframe-player';
 
 import WarningMessageComponent from './WarningMessageComponent';
-import youtubeHelper from '../helpers/youtube_helper';
 
 const YoutubePopupPlayerComponent = (props) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [hasInternet, setHasInternet] = React.useState(true)
   const [hasErrorNetwork, setHasErrorNetwork] = useState(false);
-  // const isLoadingRef = useRef(isLoading);
-  // const hasInternetRef = useRef(props.hasInternet);
+  const isLoadingRef = useRef(isLoading);
+  const hasInternetRef = useRef(true);
   useEffect(() => {
     let timeout = null
     if (props.modalVisible) {
+      NetInfo.fetch().then(state => {
+        if (hasInternet != state.isConnected)
+          setHasInternet(state.isConnected)
+      });
       setIsLoading(true);
       setHasErrorNetwork(false);
-      // hasInternetRef.current = props.hasInternet
-      // isLoadingRef.current = true
-      // timeout = setTimeout(() => {
-      //   setHasErrorNetwork(hasInternetRef.current && isLoadingRef.current);
-      // }, 25000);
+      hasInternetRef.current = hasInternet
+      isLoadingRef.current = true
+      timeout = setTimeout(() => {
+        setHasErrorNetwork(hasInternetRef.current && isLoadingRef.current);
+      }, 25000);
     }
 
     return () => !!timeout && clearTimeout(timeout)
@@ -32,39 +36,29 @@ const YoutubePopupPlayerComponent = (props) => {
       props.closeModal()
   }
 
-  // const onReady = () => {
-  //   setIsLoading(false)
-  //   isLoadingRef.current = false;
-  // }
+  const onReady = () => {
+    setIsLoading(false)
+    isLoadingRef.current = false;
+  }
 
   const renderContent = () => {
-    if (!!props.videoUrl && props.hasInternet && !hasErrorNetwork)
+    if (!!props.videoUrl && hasInternet && !hasErrorNetwork)
       return (
         <View style={{height: '100%', width: "100%", justifyContent: 'center'}}>
-          { (props.hasInternet && isLoading) &&
-            <ActivityIndicator size="large" color={props.indicatorColor || '#ffffff'} style={{position: 'absolute', alignSelf: 'center'}} />
-          }
-          {/* <YoutubePlayer
-            height={'100%'}
-            play={true}
-            videoId={youtubeHelper.getVideoId(props.videoUrl)}
-            onReady={() => onReady()}
-            webViewProps={{
-              containerStyle: {paddingTop: props.playerPaddingTop || '55%'}
-            }}
-          /> */}
-
           <YoutubeIframePlayer
-            videoUrl='https://www.youtube.com/watch?v=f7OPcDX_LyI&t=3s'
-            height={'100%'}
+            videoUrl={props.videoUrl}
+            height={props.height}
             width='100%'
-            locale='km'
-            durationFontSize={15}
+            locale={props.locale}
+            durationFontSize={props.durationFontSize}
+            loadingColor={props.indicatorColor || '#ffffff'}
+            isTablet={props.isTablet}
+            onReady={() => onReady()}
           />
         </View>
       )
 
-    return <WarningMessageComponent locale={props.locale} hasInternet={props.hasInternet} hasErrorNetwork={hasErrorNetwork} closeModal={() => props.closeModal()}
+    return <WarningMessageComponent locale={props.locale} hasInternet={hasInternet} hasErrorNetwork={hasErrorNetwork} closeModal={() => props.closeModal()}
               iconColor={props.iconColor} messageLabelStyle={props.messageLabelStyle} messageIconSize={props.messageIconSize} closeButtonStyle={props.closeButtonStyle}
            />
   }
